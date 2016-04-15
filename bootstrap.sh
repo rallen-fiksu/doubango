e3d143e169049578b811df297323f8e9d280ac84
rallen-fiksu <rallen@fiksu.com>
2016-04-15 17:05:29 -0400

    yum updates sucessful. Not testing cloning a repo and building a library.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-yum update -y
-yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig
+# Execute scrip like so:
+# sh work/bootstrap.sh
+#yum update -y
+#yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig
 
+# Build libsrtp - Recommended for WebRTC SIP clients.
+git clone https://github.com/cisco/libsrtp/
+cd libsrtp
+CFLAGS="-fPIC" ./configure --enable-pic && make && make install
