e634082b952ce4492a92a88e83c661309121537a
rallen-fiksu <rallen@fiksu.com>
2016-04-15 23:13:49 -0400

    added vim and nano

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 # Execute scrip like so:
 # sh doubango/bootstrap.sh
 yum update -y
-yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig #nano vim
+yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig nano vim
 
 # Build libsrtp - Recommended for WebRTC SIP clients.
 #git clone https://github.com/cisco/libsrtp/
