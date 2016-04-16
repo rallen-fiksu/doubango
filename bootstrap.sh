a3cbc6f0b6cf48fb4621eccb374d397dc2d3be6a
rallen-fiksu <rallen@fiksu.com>
2016-04-15 23:25:50 -0400

    added new speex and speexdsp

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,15 @@
 #yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig nano vim
 
 # Build libsrtp - Recommended for WebRTC SIP clients.
-#git clone https://github.com/cisco/libsrtp/
-#cd libsrtp
-#CFLAGS="-fPIC" ./configure --enable-pic && make && make install
+git clone https://github.com/cisco/libsrtp/
+cd libsrtp
+CFLAGS="-fPIC" ./configure --enable-pic && make && make install
 
 # OpenSSL version 1.0.1 is already installed by default in Centos 6.7 +
 
-yum -y install libogg-devel libvorbis-devel libtheora-devel
+yum -y install libogg-devel libvorbis-devel libtheora-devel speex-devel
+
+wget http://downloads.xiph.org/releases/speex/speex-1.2beta3.tar.gz
+tar -xvzf speex-1.2beta3.tar.gz
+cd speex-1.2beta3
+./configure --disable-oggtest --without-libogg && make && make install
