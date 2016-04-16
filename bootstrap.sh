81cf5b076c7959f8c9a97a56e04beb06063a6d15
rallen-fiksu <rallen@fiksu.com>
2016-04-15 23:05:18 -0400

    testing new installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Execute scrip like so:
-# sh work/bootstrap.sh
-#yum update -y
-#yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig
+# sh doubango/bootstrap.sh
+yum update -y
+yum install -y  make libtool autoconf subversion git wget cmake gcc gcc-c++ pkgconfig #nano vim
 
 # Build libsrtp - Recommended for WebRTC SIP clients.
-git clone https://github.com/cisco/libsrtp/
-cd libsrtp
-CFLAGS="-fPIC" ./configure --enable-pic && make && make install
+#git clone https://github.com/cisco/libsrtp/
+#cd libsrtp
+#CFLAGS="-fPIC" ./configure --enable-pic && make && make install
